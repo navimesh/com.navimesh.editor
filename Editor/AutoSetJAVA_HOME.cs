b1fbbe06766d41a9c3eeaf7b7e801ab489f2382c
thaihapipu <github@hanguyen.com>
2021-08-21 22:42:51 +0700

    rename folder Java home medicine

---
+++ Editor/AutoSetJAVA_HOME.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class AutoSetJAVA_HOME : Editor
+{
+    static AutoSetJAVA_HOME()
+    {
+        string newJDKPath = EditorApplication.applicationPath.Replace("Unity.exe", "Data/PlaybackEngines/AndroidPlayer/OpenJDK");
+
+        if (Environment.GetEnvironmentVariable("JAVA_HOME") != newJDKPath)
+        {
+            Environment.SetEnvironmentVariable("JAVA_HOME", newJDKPath);
+        }
+    }
+}
